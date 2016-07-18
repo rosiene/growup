34838b065fec419e1c27576bd716d33b45bdd6b7
Rosiene <rosiene.dilly@gmail.com>
2016-07-18 23:12:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(3000, function() {
+  console.log('Server listening on port 3000...');
+});
