cfdfa1109848571409983f39543c05c79b0bc854
Rosiene <rosiene.dilly@gmail.com>
2016-07-25 19:00:15 +0200

    remove sqlite and save data localy

--- app.js
+++ app.js
@@ -2,182 +2,84 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var sqlite3 = require('sqlite3').verbose();
-var fs = require('fs');
 
-var dbFile = 'game.db';
-var dbExists = fs.existsSync(dbFile);
-var db = new sqlite3.Database(dbFile);
+var circles = [];
+var players = [];
+var ranking = [];
 
-if (!dbExists) {
-  console.log("Creating database file");
-  fs.openSync(dbFile, "w");
-    createCircle();
-    createPlayer();
-    setTimeout(function() {
-      createAllFood()
-    }, 50);
+if(circles.length === 0){
+  createAllFood();
 }
 
-function createCircle(){
-  db.run("CREATE TABLE circles (" +
-          "id INT PRIMARY KEY, " +
-          "r INT, " +
-          "cx INT, " +
-          "cy INT, " +
-          "fill TEXT, " +
-          "type TEXT)");
+function getCircles(callback){
+  callback(circles);
 }
 
-function createPlayer(){
-  db.run("CREATE TABLE players (" +
-          "id INT PRIMARY KEY, " +
-          "id_circle INT REFERENCES circles(id), " +
-          "name TEXT, " +
-          "score INT, " +
-          "delay INT, " +
-          "alive INT)");
+function getPlayers(callback){
+  callback(players);
 }
 
 function insertCircle(circle) {
-  db.run("INSERT INTO circles VALUES (?, ?, ?, ?, ?, ?)",
-    [circle.id
-      , circle.r
-      , circle.cx
-      , circle.cy
-      , circle.fill
-      , circle.type
-    ]
-  );
+  circles.push(circle);
 }
 
 function insertPlayer(player) {
-  db.run("INSERT INTO players VALUES (?, ?, ?, ?, ?, ?)",
-    [player.id
-      , player.id_circle
-      , player.name
-      , player.score
-      , player.delay
-      , player.alive
-    ]
-  );
-
+  players.push(player);
 }
 
-function updateCircle(circle) {
-  db.run("UPDATE circles SET r = ?, cx =  ?, cy =  ?, fill = ? " +
-          "WHERE id = ?",
-    [circle.r
-      , circle.cx
-      , circle.cy
-      , circle.fill
-      , circle.id
-    ]
-  );
-}
-
-function updatePlayer(player) {
-  db.run("UPDATE players SET score = ?, delay = ?, alive = ? " +
-          "WHERE id = ?",
-      [player.score
-      , player.delay
-      , player.alive
-      , player.id
-    ]
-  );
-}
-
-function getCircles(callback) {
-  db.all("SELECT circles.* FROM circles " +
-           "LEFT JOIN players ON circles.id = players.id_circle " +
-          "WHERE players.alive = 1 OR circles.type = 'FOOD'"
-          , function(err, circles) {
-    if (err) {
-      console.log("getCircles error: " + err);
-      return;
+function updateCircle(circleChanged) {
+  circles = circles.map(function(circle){
+    if (circleChanged.id === circle.id){
+      return circleChanged;
+    }else{
+      return circle;
     }
-    callback(circles);
   });
+}
+
+function updatePlayer(playerChanged) {
+  players = players.map(function(player){
+    if (playerChanged.id === player.id){
+      return playerChanged;
+    }else{
+      return player;
+    }
+  });
+}
+
+function newCircle(circle, callback){
+  circle.id = getNextIdCircle();
+  insertCircle(circle);
+  callback(circle);
+}
+
+function getNextIdCircle(){
+  var max = 0;
+  circles.map(function(circle){
+    if (circle.id > max)
+      max = circle.id;
+  });
+  return parseInt(max) + 1;
+}
+
+function newPlayer(player, callback){
+  player.id = getNextIdPlayer();
+  insertPlayer(player);
+  callback(player);
+}
+
+function getNextIdPlayer(){
+  var max = 0;
+  players.map(function(player){
+    if (player.id > max)
+      max = player.id;
+  });
+  return parseInt(max) + 1;
 }
 
 function load(){
-  getCircles(function(circles) {
-    if (!circles) circles = [];
-    io.emit('circles', circles);
-  });
-
-  getPlayers(function(players) {
-    if (!players) players = [];
-    io.emit('players', players);
-  });
-}
-
-function getPlayer(player, callback) {
-  console.log(player)
-  db.all("SELECT * FROM players " +
-          "WHERE id = ? ", [player.id]
-          , function(err, players) {
-    if (err) {
-      console.log("getPlayers error: " + err);
-      return;
-    }
-    callback(players[0]);
-  });
-}
-
-function getPlayers(callback) {
-  db.all("SELECT * FROM players"
-          , function(err, players) {
-    if (err) {
-      console.log("getPlayers error: " + err);
-      return;
-    }
-    callback(players);
-  });
-}
-
-function newPlayerAndCircle(name, callback){
-
-  var newCircle = {};
-  var newPlayer = {};
-
-  setCircle(function(circle){
-    insertCircle(circle);
-    newCircle = circle;
-    setPlayer(newCircle, name, function(player){
-      insertPlayer(player);
-      newPlayer = player;
-      callback(newPlayer, newCircle);
-    });
-  });
-}
-
-function setCircle(callback){
-  db.all("SELECT (MAX(circles.id) + 1) id FROM circles ", function(err, next) {
-    if (err) {
-      console.log("getNextIdCircle error: " + err);
-      return;
-    }
-    if (next[0].id === null){
-      next[0].id = 1;
-    }
-    var circle = {id: next[0].id, r: 20, cx: 500, cy: 300, fill: randomColors(), type: "PLAYER" };
-    callback(circle);
-  });
-}
-
-function setPlayer(circle, name, callback){
-  db.all("SELECT (MAX(players.id) + 1) id FROM players ", function(err, next) {
-    if (err) {
-      console.log("getNextIdPlayers error: " + err);
-      return;
-    }
-    if (next[0].id === null){
-      next[0].id = 1;
-    }
-    var player = {id: next[0].id, id_circle: circle.id, name: name, score: 0, delay: 1, alive: 1 };
-    callback(player);
-  });
+  io.emit('circles', circles);
+  io.emit('players', players);
 }
 
 function createAllFood(){
@@ -223,21 +125,27 @@ io.on('connection', function(socket) {
     socket.emit('players', players);
   });
 
-  socket.on('newPlayerAndCircle', function (name, circles) {
-    newPlayerAndCircle(name, function(player, circle){
-      socket.emit('player-circle', player, circle);
+  socket.on('newCircle', function(circle){
+    newCircle(circle, function(newCircle){
+      socket.emit('circle', newCircle);
     });
   });
 
-  socket.on('updateCircle', function (circle) {
+  socket.on('newPlayer', function(player){
+    newPlayer(player, function(newPlayer){
+      socket.emit('player', newPlayer);
+    });
+  });
+
+  socket.on('updateCircle', function(circle){
     updateCircle(circle);
   });
 
-  socket.on('updatePlayer', function (player) {
+  socket.on('updatePlayer', function(player){
     updatePlayer(player);
   });
 
-  socket.on('load', function () {
+  socket.on('load', function(){
     load();
   });
 
